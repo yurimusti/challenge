8a0c8afdd4f80c7c7d49b1db1ccea4488302b6a1
Jordy Alcides <jordy.alcides@gmail.com>
2022-05-04 18:46:38 -0300

    Updating Banner component to use rem values; Including styles for the mobile version;

--- src/container/Banner/index.jsx
+++ src/container/Banner/index.jsx
@@ -1,53 +1,74 @@
 import React from 'react'
-import { Flex, Text, Image, Box } from '@chakra-ui/react'
-import Button from '@components/button'
-import Input from '@components/input'
+import { Button, Flex, Text, Image, Input, Box } from '@chakra-ui/react'
 
 const BannerContainer = ({}) => {
   return (
-    <>
+    <Box maxW='1200rem' mx={{ base: '0', m: 'auto' }}>
       <Box
         backgroundImage='/assets/images/logo.png'
-        height='5rem'
-        width='100%'
+        height='50rem'
+        width='198rem'
         backgroundRepeat='no-repeat'
-        marginTop='2rem'
-      />
-      <Image
-        src='/assets/images/robotInitial.png'
-        maxWidth='21rem'
-        maxHeight='46rem'
-        position='absolute'
-        marginRight='10rem'
-        top='0'
-        right='0'
+        backgroundSize='contain'
+        mt={{ base: '27rem', m: '59rem' }}
+        mx={{ base: 'auto', m: '0' }}
       />
       <Flex
         backgroundImage='/assets/images/shapeInitial.png'
         backgroundRepeat='no-repeat'
-        height='70%'
-        width='100%'
-        backgroundSize='contain'
-        flexDirection='column'
+        w='100%'
+        h='458rem'
+        backgroundSize='100% 100%'
+        backgroundPosition='center'
+        direction={{ base: 'column', m: 'row' }}
         alignItems='center'
         justifyContent='center'
-        paddingTop='4rem'
-        paddingRight='16rem'
+        pl={{ base: '0', m: '100rem' }}
+        mt='70rem'
       >
-        <Image src='/assets/images/welcomeText.png' width='40rem' />
+        <Image
+          order={{ base: '1', m: '0' }}
+          src='/assets/images/welcomeText.png'
+          width={{ base: '260rem', m: '696rem' }}
+        />
+        <Image
+          src='/assets/images/robotInitial.png'
+          h={{ base: '335rem', m: '640rem' }}
+          position={{ base: 'static', m: 'relative' }}
+          bottom='90rem'
+          mt={{ base: '-85rem', m: '0' }}
+        />
       </Flex>
-      <Flex flex={1} paddingBottom='2rem' marginLeft='1rem' marginRight='3rem'>
-        <Flex flex={2} justifyContent='center' alignItems='center'>
-          <Text fontSize='1.2rem'>JOIN THE WHITELIST</Text>
+      <Flex
+        direction={{ base: 'column', m: 'row' }}
+        justify={{ base: 'center', m: 'space-between' }}
+        align='center'
+        px={{ base: '8.5rem', m: '30rem' }}
+        mt={{ base: '37rem', m: '46rem' }}
+      >
+        <Flex>
+          <Text fontWeight={800} fontSize='18rem'>
+            JOIN THE WHITELIST
+          </Text>
         </Flex>
-        <Flex flex={3} justifyContent='center' alignItems='center' marginRight='0.6rem'>
-          <Input placeholder='Enter your email' />
-        </Flex>
-        <Flex flex={1} justifyContent='center' alignItems='center'>
-          <Button>JOIN NOW</Button>
+        <Flex
+          direction={{ base: 'column', m: 'row' }}
+          w={{ base: '100%', m: '780rem' }}
+          gap={{ base: '25rem', m: '4rem' }}
+          mt={{ base: '19rem', m: '0' }}
+        >
+          <Input
+            w='100%'
+            maxW={{ base: 'none', m: '505rem' }}
+            h='60rem'
+            placeholder='Enter your email'
+          />
+          <Button w={{ base: '100%', m: '264rem' }} h='60rem'>
+            JOIN NOW
+          </Button>
         </Flex>
       </Flex>
-    </>
+    </Box>
   )
 }
 
