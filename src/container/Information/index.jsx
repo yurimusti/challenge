56e80a34b421e38b70cab91a4353dd7ec238c0fc
Yuri Mustifaga <yurimusti@gmail.com>
2022-05-04 17:11:45 -0300

    fix: implements mobile version in download section

--- src/container/Information/index.jsx
+++ src/container/Information/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Flex, Text, Image, Box } from '@chakra-ui/react'
+import { Flex, Text, Image, Box, useBreakpointValue } from '@chakra-ui/react'
 import Lottie from 'react-lottie'
 import animationData from '../../assets/lottie/animationText01.json'
 import Button from '@components/button'
@@ -10,29 +10,49 @@ const BannerContainer = ({}) => {
     autoplay: true,
     animationData,
   }
+
+  const srcImage = useBreakpointValue({
+    base: '/assets/images/mobile/cellphones.png',
+    m: '/assets/images/cellphones.png',
+  })
+
+  console.log(srcImage)
+
   return (
     <Flex paddingTop='4rem' flexDirection='column'>
       <Lottie options={defaultOptions} height='120px' />
       <Flex
-        marginTop='6rem'
+        marginTop='64rem'
         flexDirection='column'
         justifyContent='center'
         alignItems='center'
+        textAlign={{ base: 'center', m: 'justify' }}
       >
-        <Text fontSize='2rem' textTransform='uppercase'>
+        <Text
+          fontSize='32rem'
+          fontWeight='700'
+          lineHeight='130%'
+          textTransform='uppercase'
+        >
           The only nft collection that
         </Text>
-        <Text fontSize='2rem' marginTop='1rem' textTransform='uppercase'>
+        <Text
+          fontSize='32rem'
+          fontWeight='700'
+          lineHeight='130%'
+          letterSpacing='0.1em'
+          textTransform='uppercase'
+        >
           gives you acces klooma!
         </Text>
       </Flex>
-      <Flex flexDirection='row' marginTop='5rem'>
+      <Flex flexDirection={{ base: 'column', m: 'row' }} marginTop='64rem'>
         <Text
-          fontSize='.8rem'
-          fontWeight=''
-          paddingLeft='10rem'
+          fontSize={{ base: '12rem', m: '16rem' }}
+          paddingLeft={{ base: '24rem', m: '160rem' }}
           letterSpacing='0.1em'
           lineHeight='190%'
+          textAlign='center'
         >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in
           ante et felis varius dapibus non sit amet massa. Nam posuere lacus
@@ -40,11 +60,13 @@ const BannerContainer = ({}) => {
           quam at posuere. Phasellus neque elit, ornare at faucibus
         </Text>
         <Text
-          fontSize='.8rem'
-          paddingLeft='2rem'
-          paddingRight='10rem'
+          marginTop={{ base: '24rem', m: '0rem' }}
+          fontSize={{ base: '12rem', m: '16rem' }}
+          paddingLeft='16rem'
+          paddingRight={{ base: '24rem', m: '160rem' }}
           letterSpacing='0.1em'
           lineHeight='190%'
+          textAlign='center'
         >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean in
           ante et felis varius dapibus non sit amet massa. Nam posuere lacus
@@ -52,15 +74,15 @@ const BannerContainer = ({}) => {
           quam at posuere. Phasellus neque elit, ornare at faucibus
         </Text>
       </Flex>
-      <Flex justifyContent='center' alignItems='center' marginTop='4rem'>
+      <Flex justifyContent='center' alignItems='center' marginTop='64rem'>
         <Button>Download Klooma</Button>
       </Flex>
-      <Flex width='100%' justifyContent='center' marginTop='5rem'>
+      <Flex width='100%' justifyContent='center' marginTop='64rem'>
         <Box>
           <Image
-            src='/assets/images/cellphones.png'
-            width='100%'
-            height='100%'
+            src={srcImage}
+            width={{ base: '100%', m: '100%' }}
+            height={{ base: '100%', m: '100%' }}
           />
         </Box>
       </Flex>
